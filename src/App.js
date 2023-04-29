f014c35d9793d4559e7a45572dfb546c761fcc8c
fernandoGalarcio <fdowannabe@gmail.com>
2023-04-29 14:24:57 -0500

    add redux think for fetching data

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import Navbar from './Components/Navbar';
 import CartContainer from './Components/CartContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import Modal from './Components/Modal';
 
 function App() {
-  const { cartItems } = useSelector(store => store.cart);
+  const { cartItems, isLoading } = useSelector(store => store.cart);
   const { isOpen } = useSelector(store => store.modal);
   const dispatch = useDispatch();
+
+  useEffect( () => {
+    dispatch(getCartItems())
+  }, [])
 
   useEffect(() => {
       dispatch(calculateTotals());
   }, [cartItems])
-
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
     <main>
       {isOpen && <Modal />}
