c498b4a52f0bf415d411f24563e4295b202c38e9
fernandoGalarcio <fdowannabe@gmail.com>
2023-04-29 14:11:58 -0500

    add modal for clearing cart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Modal from './Components/Modal';
 
 function App() {
   const { cartItems } = useSelector(store => store.cart);
+  const { isOpen } = useSelector(store => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,7 +17,7 @@ function App() {
 
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar/>
       <CartContainer />
     </main>
--- src/Components/CartContainer.jsx
+++ src/Components/CartContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import CartItem from './CartItem'
 import { useDispatch, useSelector } from 'react-redux'
-import { clearCart } from '../features/cart/cartSlice'
+
+import { openModal } from '../features/modal/modalSlice'
 
 const CartContainer = () => {
   const { cartItems, total, amount } = useSelector(store => store.cart)
@@ -35,7 +36,7 @@ const CartContainer = () => {
           <h4>total <span>${total.toFixed(2)}</span></h4>
         </div>
         <button
-          onClick={() => dispatch(clearCart)}
+          onClick={() => dispatch(openModal())}
           className='btn clear-btn'>
             clear cart
         </button>
